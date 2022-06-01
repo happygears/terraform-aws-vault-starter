77b0912f2a4aeeae3a5aab9127b3e0e57ecf9469
Alexander Komlik <akomlik@happygears.net>
2022-06-01 15:53:56 +0200

    output vault_instance_role_arn and vault_snapashots_bucket_id

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -1,3 +1,9 @@
 output "aws_iam_instance_profile" {
   value = aws_iam_instance_profile.vault.name
 }
+
+data "aws_caller_identity" "current" {}
+
+output "aws_iam_role_arn" {
+  value = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${aws_iam_instance_profile.vault.role}"
+}
--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,11 @@ output "vault_target_group_arn" {
   description = "Target group ARN to register Vault nodes with"
   value       = module.loadbalancer.vault_target_group_arn
 }
+
+output "vault_instance_role_arn" {
+  value = module.iam.aws_iam_role_arn
+}
+
+output "vault_snapashots_bucket_id" {
+  value = var.enable_snapshots ? module.snapshots.s3_bucket_id : ""
+}
