7f0a5393a5c6e05dc9a8a9a8457449090d611810
Thomas Nicollet <thomas.nicollet@nebulis.io>
2022-06-30 14:23:14 +0200

    Changed CA common name to avoid issue of the server certificate being invalid

--- examples/aws-secrets-manager-acm/tls.tf
+++ examples/aws-secrets-manager-acm/tls.tf
@@ -9,7 +9,7 @@ resource "tls_self_signed_cert" "ca" {
   private_key_pem = tls_private_key.ca.private_key_pem
 
   subject {
-    common_name = "vault.server.com"
+    common_name = "ca.vault.server.com"
   }
 
   validity_period_hours = 87600 # 10 years
